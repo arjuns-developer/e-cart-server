4887d55beece134177ed9f425bfbacf1c5848f84
arjun <arjun.surendran@qburst.com>
2023-01-02 14:22:51 +0530

    added cors libary

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const jsonServer = require('json-server');
+const cors = require('cors');
+
 const server = jsonServer.create();
 const router = jsonServer.router('db.json'); // <== Will be created later
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3200; // <== You can change the port
 
 server.use(middlewares);
+server.use(cors())
+
+
 server.use(router);
 
-server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Headers', '*')
-    next()
-  })
 
 server.listen(port);
