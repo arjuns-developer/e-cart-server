82257046faaf1b242d4d8b3509e062a199633783
arjun <arjun.surendran@qburst.com>
2023-01-02 12:54:56 +0530

    added cors config

--- server.js
+++ server.js
@@ -7,4 +7,10 @@ const port = process.env.PORT || 3200; // <== You can change the port
 server.use(middlewares);
 server.use(router);
 
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', '*')
+    next()
+  })
+
 server.listen(port);
